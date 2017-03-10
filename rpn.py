82bde5bc5e2a67eb12e71b6049c066fcc2bd7d82
James T Obermaier <jobermai@caen-vnc-vm16.engin.umich.edu>
2017-03-10 14:43:50 -0500

    dding subtraction

--- rpn.py
+++ rpn.py
@@ -1,11 +1,34 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+def add(arg1, arg2):
+	return arg1 + arg2
+
+def subtract(arg1, arg2):
+	return arg1 - arg2
+
+operators = {
+	'+': add,
+	'-': subtract,
+}
 
 def calculate(arg):
-	pass
+	stack = list()
+	for operand in arg.split():	
+		try:
+			operand = float(operand)	
+			stack.append(operand)
+		except:
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator = operators[operand]
+			result = operator(arg1, arg2)
+			stack.append(result)			
+	return stack.pop()
+
 def main():
 	while True:
-		calculate(input('rpn calc> '))
+		result = calculate(input('rpn calc> '))
+		print ("Result:", result)
 
 if __name__ == '__main__':
 	main()
-
